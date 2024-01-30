e7156b1bb1f47f49449f68de7e702342934eaea9
Lior Abuhav <t-liorabuhav@microsoft.com>
2024-01-31 01:58:55 +0200

    first example query - ratio home / away

--- Data Ingestion/games.py
+++ Data Ingestion/games.py
@@ -4,25 +4,28 @@ from cassandra.cqlengine import connection
 from cassandra.cqlengine.management import sync_table
 import csv
 
+def get_valid_number(number: str):
+    if number.isdigit():
+        return int(number)
+    
+    if number[:-2].isdigit():
+        return int(number[:-2])
+    
+    return 0
+
 class Game(Model):
     game_id = columns.Integer(primary_key=True)
     game_date = columns.Date()
-    home_team_id = columns.Integer()
-    visitor_team_id = columns.Integer()
-    season = columns.Integer()
-    pts_home = columns.Integer()
-    fg_pct_home = columns.Float()
-    ft_pct_home = columns.Float()
-    fg3_pct_home = columns.Float()
-    ast_home = columns.Integer()
-    reb_home = columns.Integer()
-    pts_away = columns.Integer()
-    fg_pct_away = columns.Float()
-    ft_pct_away = columns.Float()
-    fg3_pct_away = columns.Float()
-    ast_away = columns.Integer()
-    reb_away = columns.Integer()
-    home_team_wins = columns.Boolean()
+    season = columns.Integer(partition_key=True)
+    team_id = columns.Integer(partition_key=True)
+    pts = columns.Integer()
+    fg_pct = columns.Float()
+    ft_pct = columns.Float()
+    fg3_pct = columns.Float()
+    ast = columns.Integer()
+    reb = columns.Integer()
+    is_win = columns.Boolean()
+    is_home_team = columns.Boolean(partition_key=True)
 
     __table_name__ = "Games"
 
@@ -41,27 +44,34 @@ for game in games_mapping:
         if value == '':
             game[key] = None
 
-    team_model = Game.create(
+    game_home_model = Game.create(
         game_id = game['GAME_ID'],
         game_date = game['GAME_DATE_EST'],
-        home_team_id = game['HOME_TEAM_ID'],
-        visitor_team_id = game['VISITOR_TEAM_ID'],
+        team_id = game['HOME_TEAM_ID'],
         season = game['SEASON'],
-        pts_home = game['PTS_home'] if isinstance(game['PTS_home'], int) else 0,
-        fg_pct_home = game['FG_PCT_home'],
-        ft_pct_home = game['FT_PCT_home'],
-        fg3_pct_home = game['FG3_PCT_home'],
-        ast_home = game['AST_home'] if isinstance(game['AST_home'], int) else 0,
-        reb_home = game['REB_home'] if isinstance(game['REB_home'], int) else 0,
-        pts_away = game['PTS_away'] if isinstance(game['PTS_away'], int) else 0,
-        fg_pct_away = game['FG_PCT_away'],
-        ft_pct_away = game['FT_PCT_away'],
-        fg3_pct_away = game['FG3_PCT_away'],
-        ast_away = game['AST_away'] if isinstance(game['AST_away'], int) else 0,
-        reb_away = game['REB_away'] if isinstance(game['REB_away'], int) else 0,
-        home_team_wins = game['HOME_TEAM_WINS']
+        pts = get_valid_number(game['PTS_home']),
+        fg_pct = game['FG_PCT_home'],
+        ft_pct = game['FT_PCT_home'],
+        fg3_pct = game['FG3_PCT_home'],
+        ast = get_valid_number(game['AST_home']),
+        reb = get_valid_number(game['REB_home']),
+        is_win = game['HOME_TEAM_WINS'],
+        is_home_team = True
+    )
+    game_away_model = Game.create(
+        game_id = game['GAME_ID'],
+        game_date = game['GAME_DATE_EST'],
+        team_id = game['VISITOR_TEAM_ID'],
+        season = game['SEASON'],
+        pts = get_valid_number(game['PTS_away']),
+        fg_pct = game['FG_PCT_away'],
+        ft_pct = game['FT_PCT_away'],
+        fg3_pct = game['FG3_PCT_away'],
+        ast = get_valid_number(game['AST_away']),
+        reb = get_valid_number(game['REB_away']),
+        is_win = not game['HOME_TEAM_WINS'],
+        is_home_team = False
     )
     print(f"Created game {game['GAME_ID']}")
 
 print(f"Created {Game.objects.count()} games.")
-
